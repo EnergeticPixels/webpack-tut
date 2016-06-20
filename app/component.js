ec36bdcf8a770970726842f31aa8304bd2718afd
Anthony Jackman <energeticpixels@gmail.com>
2016-06-20 18:58:43 -0500

    local scoped styles

--- app/component.js
+++ app/component.js
@@ -1,7 +1,11 @@
+var styles = require('./main.css');
+
 module.exports = function() {
     var element = document.createElement('h1');
 
     element.innerHTML = 'Hello Tony';
 
+    element.className = styles.redButton;
+    
     return element;
 };
